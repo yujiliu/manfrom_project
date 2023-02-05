40e541d032db0ba794ccfb44389556a08bab1cba
yevheniiliushenko <evegeniylyushenko@gmail.com>
2023-02-05 21:57:20 +0200

    Moved to osx

--- main.py
+++ main.py
@@ -1,16 +1,68 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import operator
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+wrong_symbols = ['\'', '\"', "\n"]
+lines = []
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def clear_string(string):
+    new_string = ''
+    for symbol in string:
+        if not symbol in wrong_symbols:
+            new_string += symbol
+    return new_string
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+def create_dict(main_list, current_list):
+    i = 0
+    temp_dict = dict.fromkeys(main_list)
+    for key in temp_dict.keys():
+        try:
+            temp_dict[key] = current_list[i]
+            i += 1
+        except IndexError:
+            temp_dict[key] = ' None'
+    return temp_dict
+
+
+def sort_data_by(data, keyword, reverse):
+    sorted_data = sorted(data, key=lambda x: x[keyword].lower(), reverse=reverse)
+    print(f'Sorted by {keyword}:')
+    print("\n".join(map(str, sorted_data)), "\n")
+
+
+def main():
+    with open("test.csv", encoding="utf-8") as main_file:
+        name_string = main_file.readline()[:-1]
+        print(name_string)
+
+        main_list = [element for element in name_string.split(",")]
+        print(main_list)
+
+        data = []
+
+        for line in main_file:
+
+            correct_string = clear_string(line)
+
+            number = correct_string.split(",")[0]
+            print(f'{number} -- >', correct_string)
+            temp_list = []
+            for element in correct_string[:-1].split(","):
+                temp_list.append(element)
+            print(temp_list)
+
+            temp_dict = create_dict(main_list, temp_list)
+            print(temp_dict, "\n")
+            data.append(temp_dict)
+
+    print("\n".join(map(str, data)), "\n")
+
+    keyword = 'customers_lastname'  # Change sort keyword here
+    reverse = False  # Is sort reversed?
+
+    sort_data_by(data, keyword, reverse)
+
+
+if __name__ == "__main__":
+    main()
